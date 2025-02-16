2ee54b048b348787ad6e5a93462b73b6fe469e07
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2025-02-16 11:37:05 +0900

    fix: sample code fix

--- src/App.vue
+++ src/App.vue
@@ -4,23 +4,17 @@ import { useSampleStore } from './stores/sampleStore'
 import { useCounterStore } from './stores/counterStore'
 
 const sampleStore = useSampleStore()
-const { count: countSample, doubleCount: doubleCountSample } = storeToRefs(sampleStore)
-const { increment: incrementSample } = sampleStore
+const { count: countSample } = storeToRefs(sampleStore)
 
 const counterStore = useCounterStore()
-const { count: countCounter, doubleCount: doubleCountCounter } = storeToRefs(counterStore)
-const { increment: incrementCounter } = counterStore
+const { count: countCounter } = storeToRefs(counterStore)
 </script>
 
 <template>
   <div>
     <h1>Sample</h1>
     <div>count: {{ countSample }}</div>
-    <div>double count: {{ doubleCountSample }}</div>
-    <button @click="incrementSample">Increment</button>
     <h2>Counter</h2>
     <div>count: {{ countCounter }}</div>
-    <div>double count: {{ doubleCountCounter }}</div>
-    <button @click="incrementCounter">Increment</button>
   </div>
 </template>
